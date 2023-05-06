f5f2208a2234571ef1ca9f71cddd7923054ea9ae
Nirob Hasan <nirob427417@gmail.com>
2023-05-06 10:56:11 +0600

    pratice

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const course = "next level web deplovment";
+console.log(course);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const course: string = "next level web development";
+console.log(course);
+
+
+//run code command
+//  npx ts-node-dev --respawn src/index.ts
